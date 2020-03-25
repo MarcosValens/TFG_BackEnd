eca96ba19f387dbe332a74203d8798189da583fc
unknown <alexis.platino@gmail.com>
2020-03-25 17:50:30 +0100

    Added basic login template. TODO: implement passport strategies

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,37 @@
 const router = require("express").Router();
 const { tokenManager } = require("./../services");
 // TODO: Add passport strategies
+router.post("/local", (req, res, next) => {
+    /* 
+        This function should be replaced with passport local strategy
+    */
+   next();
+}, (req, res) => {
+    const user = req.user;
+    const token = tokenManager.create(user);
+    res.json({token});
+});
+
+router.get("/google", (req, res, next) => {
+    /*
+        This function should be replaced with google local strategy 
+    */
+   next();
+}, (req, res) => {
+    res.status(200).end();
+});
+
+router.get("/google/callback", (req, res, next) => {
+        /*
+        This function should be replaced with google local strategy 
+    */
+   next();
+}, (req, res) => {
+    const user = req.user
+    // TODO: Get or create user with mongoose model
+    const mongooseUser = user; // Leave it like this until this is implemented
+    const token = tokenManager.create(mongooseUser);
+    res.status(200).json({token});
+});
 
 module.exports = router;
