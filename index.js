ecae9dee34fa871b08b04de19aece7e5b34c69a1
unknown <alexis.platino@gmail.com>
2020-03-25 17:29:34 +0100

    Initialized express project

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const routes = require("./src/routes");
+const port = process.env.PORT || 8000;
+
+const helmet = require("helmet");
+const cors = require("cors");
+
+const corsOptions = {
+    allowedHeaders: ["Authorization", "Accept", "Origin"],
+    allowedMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: process.env.WHITELIST || "http://localhost:8080"
+};
+
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+routes.forEach(({path, router}) => app.use(path, router));
+
+app.use((req, res) => {
+    res.status(400).json({message: "Resource not found wtf?"});
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
