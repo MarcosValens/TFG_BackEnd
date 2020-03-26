07b74464d4ef5dd4d3aea2dd4b863c0249dd4b4f
marcos <marcosvalens@gmail.com>
2020-03-26 17:44:48 +0100

    Marcos - fixed imports from network routes and networkManager

--- src/routes/network.js
+++ src/routes/network.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
-const { NetworkManager } = require("./../services/networkManager");
+const networkManager = require("./../services/networkManager");
 
 router.post("/create", (req, res) => {
-    NetworkManager.create(req.network)
+    networkManager.create(req.network)
 });
 
 router.post("/update", (req, res) => {
-    NetworkManager.update(req.network)
+    networkManager.update(req.network)
 });
 
 router.post("/delete", (req, res) => {
-    NetworkManager.delete(req.network)
+    networkManager.delete(req.network)
 });
 
