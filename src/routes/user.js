d11e56be7e2318d79240514ba40d0eb8c2f0338c
unknown <alexis.platino@gmail.com>
2020-03-26 20:40:15 +0100

    Added user router and added update method to user manager

---
+++ src/routes/user.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const passport = require("passport");
+const { userManager } = require("./../services");
+
+router.use(passport.authenticate("jwt"));
+
+router.get("/user", async (req, res) => {
+    try {
+        const user = await userManager.findByEmail(req.body.email);
+        res.status(200).json(user);
+    } catch(ex) {
+        res.status(500).json({error: "Something went wrong OOPS!"})
+    }
+});
+
+router.put("/update", async (req, res) => {
+    try {
+        const user = await userManager.findByEmail(req.user.email);
+        const didUpdate = await userManager.update(user, req.body);
+        if (!didUpdate) {
+            return res.status(400).json({error: "Check your fields"}); 
+        }
+        res.status(200).json({message: "Profile updated successfully!"});
+    } catch(error) {
+        res.status(500).json({error: "Something went wrong OOPS!"});
+    }
+})
+
+module.exports = router;
--- src/services/userManager.js
+++ src/services/userManager.js
@@ -2,32 +2,53 @@ const data = {
     instance: null
 };
 const bcrypt = require("bcrypt");
-const { User } = require("./../model/");
+const { User, Photo } = require("./../model/");
 class UserManager {
     constructor() {}
 
     async create({ email, name, surname, issuer, password = "" }) {
         try {
-            const rounds = parseInt(process.env.SALT_ROUNDS) || 10;
             const userData = { email, name, surname, createdWith: issuer };
             
             if (password) {
-                const salt = await bcrypt.genSalt(rounds);
-                const hashedPassword = await bcrypt.hash(password, salt);
+                const hashedPassword = await this._hashPassword(password);
                 userData.password = hashedPassword;
             }
             
             await (await User.create(userData)).save();
             return true;
         } catch (ex) {
-            console.log(ex);
             return null;
         }
+    }
+
+    async _hashPassword(password) {
+        const rounds = parseInt(process.env.SALT_ROUNDS) || 10;
+        const salt = await bcrypt.genSalt(rounds);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
     }
 
     async findByEmail(email) {
         const userFound = await User.findOne({ email });
         return userFound;
+    }
+
+    async update(userFromDataBase, userData) {
+        userFromDataBase.name = userData.name;
+        userFromDataBase.surname = userData.surname;
+        userFromDataBase.photo = userData.photo;
+        if (userData.password) {
+            const hashedPassword = await this._hashPassword(userData.password);
+            userFromDataBase.password = hashedPassword;
+        }
+        userFromDataBase.email = userData.email;
+        try {
+            await userFromDataBase.save();
+            return true;
+        } catch(err) {
+            return false;
+        }
     }
 }
 
