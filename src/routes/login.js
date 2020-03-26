fa654b776933c118faecc25042638c017fa461ae
unknown <alexis.platino@gmail.com>
2020-03-26 18:00:42 +0100

    Added logging and error handling when we cant create the user or issue a token

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,36 +1,60 @@
 const router = require("express").Router();
 const { tokenManager, userManager } = require("./../services");
 
-require("./../../config/passport-setup.js")
+require("./../../config/passport-setup.js");
 const passport = require("passport");
 
 router.get("/error", (req, res) => {
-    res.status(401).json({error: "What were you trying to do?"})
-})
-
-router.post("/local", passport.authenticate("local", {failureRedirect: "/login/error"}), (req, res) => {
-    const user = req.user;
-    const token = tokenManager.create(user);
-    res.json({token});
+    res.status(401).json({ error: "What were you trying to do?" });
 });
 
-router.get("/google", passport.authenticate("google", {scope: ["email", "profile"]}), (req, res) => {
-    res.status(200);
-});
-
-
-router.get("/google/callback", passport.authenticate("google"), async (req, res) => {
-    const {email, provider, name: {givenName, familyName}} = req.user;
-    let existingUser = await userManager.findByEmail(email);
-    console.log(existingUser)
-    if (!existingUser) {
-        await userManager.create({email, issuer: provider, name: givenName, surname: familyName});
+router.post(
+    "/local",
+    passport.authenticate("local", { failureRedirect: "/login/error" }),
+    (req, res) => {
+        const user = req.user;
+        const token = tokenManager.create(user);
+        res.json({ token });
     }
+);
 
-    existingUser = await userManager.findByEmail(email);
-    
-    const token = tokenManager.create(existingUser);
-    res.status(200).json({token});
-});
+router.get(
+    "/google",
+    passport.authenticate("google", { scope: ["email", "profile"] }),
+    (req, res) => {
+        res.status(200);
+    }
+);
+
+router.get(
+    "/google/callback",
+    passport.authenticate("google"),
+    async (req, res) => {
+        try {
+            const {
+                email,
+                provider,
+                name: { givenName, familyName }
+            } = req.user;
+            console.log(req.user);
+            let existingUser = await userManager.findByEmail(email);
+            if (!existingUser) {
+                await userManager.create({
+                    email,
+                    issuer: provider,
+                    name: givenName,
+                    surname: familyName
+                });
+            }
+
+            existingUser = await userManager.findByEmail(email);
+
+            const token = tokenManager.create(existingUser);
+            res.status(200).json({ token });
+        } catch(error) {
+            res.status(500).json({error: "Something went wrong"});
+        }
+    }
+);
 
 module.exports = router;
