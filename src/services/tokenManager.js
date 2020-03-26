51086aa751ffae3abc70daab76f48668e9f118c9
unknown <alexis.platino@gmail.com>
2020-03-26 18:56:52 +0100

    Added passport jwt strategy

--- src/services/tokenManager.js
+++ src/services/tokenManager.js
@@ -4,14 +4,10 @@ class TokenManager {
     constructor() {}
 
     create(user) {
-        const publicUser = {email: user.email, name: user.name, surname: user.surname, networks: user.networks, id: user._id} ;
+        const publicUser = {email: user.email, name: user.name, surname: user.surname, id: user._id} ;
         return jwt.sign(publicUser, process.env.SECRET, {
             expiresIn: "4h",
         });
-    }
-
-    validate(token) {
-        return jwt.verify(token, process.env.SECRET);
     }
 }
 const instance = {
