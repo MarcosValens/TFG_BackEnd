295475009f6ec146d5c7ac1223d2e84a1ae0eff8
marcos <marcosvalens@gmail.com>
2020-03-27 17:58:42 +0100

    Marcos - pull

--- src/middlewares/validators/port.js
+++ src/middlewares/validators/port.js
@@ -1,3 +1,9 @@
 async function validatePort (req, res, next) {
-    const portNumber = req.body.portNumber;
+    const host = req.host;
+    const port= req.body.portId;
+
+    const portId = host.ports.find((portId) => portId = port);
+    if (!portId){
+
+    }
 }
