be71bfa15b295e00236886f0edcf248102a3d76f
unknown <alexis.platino@gmail.com>
2020-03-27 19:49:30 +0100

    Fixed port validation

--- src/middlewares/validators/port.js
+++ src/middlewares/validators/port.js
@@ -2,13 +2,14 @@ const { portManager } = require("./../../services");
 
 async function validatePort (req, res, next) {
     const host = req.host;
-    const port= req.body.portId;
+    const port = req.body.portId;
 
-    const portId = host.ports.find((portId) => portId = port);
+    const portId = host.ports.find((portId) => portId.toString() === port);
     if (!portId){
         return res.status(401).json({"message": "Port not found"})
     }
-    res.port(portManager.findById(portId));
+    const portFromDataBase = await portManager.findById(portId);
+    res.port = portFromDataBase;
     next()
 }
 
