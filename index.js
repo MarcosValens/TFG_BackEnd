b561f334183f13ed6e9740c4cf3291c015994869
unknown <alexis.platino@gmail.com>
2020-05-06 17:46:31 +0200

    Still trying to fix request entity too large 413 error

--- index.js
+++ index.js
@@ -44,11 +44,12 @@ app.use(compression())
 app.use(helmet());
 app.use(cors(corsOptions));     
 
+app.use(express.json({ limit: "50mb"}));
+
 app.use(express.urlencoded({ 
     limit: "50mb",
     extended: false
 }));
-app.use(express.json({ limit: "50mb"}));
 
 app.use(passport.initialize());
 require("./config/passport-setup");
