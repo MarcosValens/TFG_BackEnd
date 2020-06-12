9491225cfd6c76eeee1dd5d47be11daebde1b9c3
unknown <alexis.platino@gmail.com>
2020-06-12 20:57:18 +0200

    Asd

--- src/routes/register.js
+++ src/routes/register.js
@@ -4,17 +4,20 @@ const { userManager } = require("./../services");
 const { register } = require("./../route-validators");
 
 router.post("/", register.register, async (req, res) => {
-    const errors = register.validate(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-    const userData = req.body;
-    userData.issuer = "local";
-    const userWasCreated = await userManager.create(userData);
-    if (!userWasCreated) {
-        return res.status(500).json({ message: "Could not create user." });
-    }
-    res.status(200).json({ success: "User created sulucesefuly" });
+    try {
+        const errors = register.validate(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        const userData = req.body;
+        userData.issuer = "local";
+        const userWasCreated = await userManager.create(userData);
+        console.log(userWasCreated);
+        if (!userWasCreated) {
+            return res.status(500).json({ message: "Could not create user." });
+        }
+        res.status(200).json({ success: "User created sulucesefuly" });
+    } catch(e) {console.log(e)}
 });
 
 module.exports = router;
